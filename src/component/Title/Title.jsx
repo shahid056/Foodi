6598562e7621c6f8fb1ade4e31655be3e6bf3957
shahid056 <shaikhs2956@gmail.com>
2024-02-17 21:58:54 +0530

    update

--- src/component/Title/Title.jsx
+++ src/component/Title/Title.jsx
@@ -5,6 +5,7 @@ function Title({ title }) {
       <h1 className="bg-[#F2EFED] text-center font-[900] text-4xl text-[#221F20] uppercase">
         {title}
       </h1>
+      <p className="text-center">Search Food By ingredients</p>
     </>
   );
 }
